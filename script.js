b4a4b4d8292882b9ec33a2f30ecefa00b934049f
baxbrown <darcy+baxter@darcybrown.com>
2021-02-19 01:58:15 +0000

    more key controls

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let config = {
-	type: Phaser.AUTO,
+  type: Phaser.AUTO,
   width: 1300,
   height: 630,
   physics: {
@@ -15,87 +15,87 @@ let config = {
   }
 };
 const game = new Phaser.Game(config);
-function preload(){
+function preload() {
   this.load.atlas("player", "assets/spritesheet.png", "assets/sprites.json");
   this.load.image("platform", "assets/platform.png");
   this.load.image("spike", "assets/spike.png");
   this.load.image("coin", "assets/coin.png");
   this.spawnPlayer = (x, y) => {
-	  this.player = this.physics.add.sprite(x, y, "player", "sprite_0");
-  	this.player.body.setGravityY(800);
-  	this.physics.add.collider(this.player, this.platforms);
-	  this.cameras.main.startFollow(this.player);
+    this.player = this.physics.add.sprite(x, y, "player", "sprite_0");
+    this.player.body.setGravityY(800);
+    this.physics.add.collider(this.player, this.platforms);
+    this.cameras.main.startFollow(this.player);
     this.player.score = 0;
-	  this.scoreText = this.add.text(0, 0, "Score: "+this.player.score, {
-		fill:"#000000",
-		fontSize:"20px",
-		fontFamily:"Arial Black"
-	}).setScrollFactor(0).setDepth(200);
+    this.scoreText = this.add.text(0, 0, "Score: " + this.player.score, {
+      fill: "#000000",
+      fontSize: "20px",
+      fontFamily: "Arial Black"
+    }).setScrollFactor(0).setDepth(200);
   }
   this.parseMap = (map) => {
     let mapArr = map.split('.');
     let drawX = 0;
     let drawY = 0;
-    mapArr.forEach(row=>{
-	drawX = 0;
-	for(let i = 0; i<row.length; i++){
-		if(row.charAt(i)==='1'){
-			this.platforms.create(drawX, drawY, "platform");
-		}else if(row.charAt(i)==='2'){
-			if(row.charAt(i+1)==='1'){
-				this.spawnPlayer(drawX-4, drawY-12);
-			}else if(row.charAt(i-1)==='1'){
-				this.spawnPlayer(drawX+4, drawY-12);
-			}else{
-				this.spawnPlayer(drawX, drawY-12);					
-			}
-		}else if(row.charAt(i)==='c'){
-			this.coins.create(drawX, drawY+10, "coin");
-        //==================================
-		}else if(row.charAt(i)==='s'){
-        	this.spikes.create(drawX, drawY+10, "spike"); 
+    mapArr.forEach(row => {
+      drawX = 0;
+      for (let i = 0; i < row.length; i++) {
+        if (row.charAt(i) === '1') {
+          this.platforms.create(drawX, drawY, "platform");
+        } else if (row.charAt(i) === '2') {
+          if (row.charAt(i + 1) === '1') {
+            this.spawnPlayer(drawX - 4, drawY - 12);
+          } else if (row.charAt(i - 1) === '1') {
+            this.spawnPlayer(drawX + 4, drawY - 12);
+          } else {
+            this.spawnPlayer(drawX, drawY - 12);
+          }
+        } else if (row.charAt(i) === 'c') {
+          this.coins.create(drawX, drawY + 10, "coin");
+          //==================================
+        } else if (row.charAt(i) === 's') {
+          this.spikes.create(drawX, drawY + 10, "spike");
         }
         //==================================
-		drawX+=40;
-	}
-	drawY+=40;
-});
+        drawX += 40;
+      }
+      drawY += 40;
+    });
   }
 }
 
-function create(){
- const map = '11111111111111111111111111.'+
-            '1               c        1.'+
-            '1    c     c     s     c 1.'+
-            '1 2  1  s  1  c  1  c  1 1.'+
-            '1 1     1     1     1    1.'+
-            '1                        1.'+
-            '1    c     c     s     s 1.'+
-            '1 c  1  s  1  c  1  c  1 1.'+
-            '1 1     1     1     1    1.'+
-            '1                        1.'+
-            '1    c     s     c     c 1.'+
-            '1 s  1  c  1  c  1  s  1 1.'+
-            '1 1     1     1     1    1.'+
-            '1                        1.'+
-            '1   c       c     c   c  1.'+
-            '11111111111111111111111111';
-  this.cameras.main.setBackgroundColor('#00F9E6');  
+function create() {
+  const map = '11111111111111111111111111.' +
+    '1               c        1.' +
+    '1    c     c     s     c 1.' +
+    '1 2  1  s  1  c  1  c  1 1.' +
+    '1 1     1     1     1    1.' +
+    '1                        1.' +
+    '1    c     c     s     s 1.' +
+    '1 c  1  s  1  c  1  c  1 1.' +
+    '1 1     1     1     1    1.' +
+    '1                        1.' +
+    '1    c     s     c     c 1.' +
+    '1 s  1  c  1  c  1  s  1 1.' +
+    '1 1     1     1     1    1.' +
+    '1                        1.' +
+    '1   c       c     c   c  1.' +
+    '11111111111111111111111111';
+  this.cameras.main.setBackgroundColor('#00F9E6');
   this.platforms = this.physics.add.staticGroup();
   this.coins = this.physics.add.group();
   this.spikes = this.physics.add.group();
   this.parseMap(map);
-  this.collectCoin = (player, coin)=>{
-	  player.score+=10;
-    this.scoreText.setText("Score: "+ this.player.score);
+  this.collectCoin = (player, coin) => {
+    player.score += 10;
+    this.scoreText.setText("Score: " + this.player.score);
     coin.destroy();
   };
-  this.die = ()=>{
-	  this.physics.pause();
+  this.die = () => {
+    this.physics.pause();
     let deathText = this.add.text(0, 0, "YOU DIED", {
-        color:"#d53636",
-        fontFamily:"Arial Black",
-        fontSize:"50px"
+      color: "#d53636",
+      fontFamily: "Arial Black",
+      fontSize: "50px"
     }).setScrollFactor(0);
     Phaser.Display.Align.In.Center(deathText, this.add.zone(650, 315, 1300, 630));
   }
@@ -103,51 +103,55 @@ function create(){
 
 
   this.physics.add.overlap(this.player, this.coins, this.collectCoin, null, this);
+  this.key_Space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
   this.key_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
   this.key_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
   this.key_D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-	
+  this.key_Up = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+  this.key_Left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+  this.key_Right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+
   this.anims.create({
-    key:"walk",
-    frames:[
+    key: "walk",
+    frames: [
       {
-        key:"player", frame:"sprite_2"
-      }, 
+        key: "player", frame: "sprite_2"
+      },
       {
-        key:"player", frame:"sprite_1"
+        key: "player", frame: "sprite_1"
       }
     ],
-    frameRate:10,
-    repeat:-1
+    frameRate: 10,
+    repeat: -1
   });
   this.anims.create({
-    key:"stand",
-    frames:[
+    key: "stand",
+    frames: [
       {
-        key:"player", frame:"sprite_0"
+        key: "player", frame: "sprite_0"
       }
     ],
-    frameRate:1
+    frameRate: 1
   });
 };
 
 
 
 
-function update(){
-  if (this.key_W.isDown && this.player.body.touching.down){
-	  this.player.setVelocityY(-550);
+function update() {
+  if ((this.key_W.isDown || this.key_Up.isDown || this.key_Space.isDown) && this.player.body.touching.down) {
+    this.player.setVelocityY(-550);
   }
-  if (this.key_A.isDown){
-	  this.player.setVelocityX(-200);
+  if (this.key_A.isDown || this.key_Right.isDown) {
+    this.player.setVelocityX(-200);
     this.player.anims.play("walk", true);
-     this.player.flipX = true;
-  } else if (this.key_D.isDown){
-	  this.player.setVelocityX(200);
+    this.player.flipX = true;
+  } else if (this.key_D.isDown || this.key_Left.isDown) {
+    this.player.setVelocityX(200);
     this.player.anims.play("walk", true);
-     this.player.flipX = false;
+    this.player.flipX = false;
   } else {
-	  this.player.setVelocityX(0);
+    this.player.setVelocityX(0);
     this.player.anims.play("stand", true);
   }
 }
